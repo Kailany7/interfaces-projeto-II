bce4748223125fca6921c8d2b328535256c7a586
Kailany7 <Kailanysilva118@gmail.com>
2026-04-15 21:00:05 -0300

    chore: remove node_modules do repositorio

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Auth/Login";
 import AgendamentoList from "../pages/Agendamento/AgendamentoList";
 import ClienteList from "../pages/Cliente/ClienteList";
+import GaleriaList from "../pages/Galeria/GaleriaList";
 import PrivateRoute from "./PrivateRoute";
 import AdminLayout from "../layouts/AdminLayout";
 
@@ -20,6 +21,7 @@ function AppRoutes() {
       >
         <Route path="agendamentos" element={<AgendamentoList />} />
         <Route path="clientes" element={<ClienteList />} />
+        <Route path="galeria" element={<GaleriaList />} />
       </Route>
     </Routes>
   );
