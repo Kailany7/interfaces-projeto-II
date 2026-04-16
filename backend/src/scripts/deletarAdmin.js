86c3bed59e46201b95f1df0ca77666af449fa298
Kailany7 <Kailanysilva118@gmail.com>
2026-04-16 15:42:12 -0300

    atualizado

--- backend/src/scripts/deletarAdmin.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-import Funcionario from "../models/funcionarioModel.js";
-
-await mongoose.connect("mongodb://localhost:27017/nomedobanco");
-await Funcionario.deleteOne({ email: "admin@gmail.com" });
-console.log("Admin deletado");
-await mongoose.disconnect();
